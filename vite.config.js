994393825d394ccebce48c4e672f9419c915c083
Zuhrdev <91844461+Elieleche@users.noreply.github.com>
2023-12-29 01:41:36 +0100

    ok

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
   base: '/',
   build: {
-    outDir: 'build', // Specify the correct output directory
+    outDir: 'dist', // Specify the correct output directory
   },
 });
